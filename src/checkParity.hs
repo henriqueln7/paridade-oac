74cb791961a2f5ffee307cd9bbb9cccaa00f4f23
DellAero <Henrique Lopes>
2021-04-25 14:02:02 -0300

    Junta todos os algoritmos em um arquivo so. Adiciona uma documentacao basica

--- src/checkParity.hs
+++
@@ -1,8 +0,0 @@
-data Parity = Even | Odd
-
--- O código pode ser visto melhor em https://github.com/henriqueln7/paridade-oac
--- Função responsável por verificar se a paridade de uma cadeia de bits (aqui foi escolhido representar cada bit como um char). É possível passar dois tipos de paridade: Even e Odd. Even retornará True caso o número de bits 1 seja par e Odd retornará True caso o número de bits seja ímpar.
-checkParity :: [Integer] -> Parity -> Bool
-checkParity bits Even = even $ sum bits
-checkParity bits Odd = odd $ sum bits
-
